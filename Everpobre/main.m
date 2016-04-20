ca65dcf2eefaaedea8a841b73d8f13ea23823be3
David de Tena <daviddetenacalvo@gmail.com>
2016-04-20 09:50:39 +0200

    Initial Commit

---
+++ Everpobre/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Everpobre
+//
+//  Created by David de Tena on 20/04/16.
+//  Copyright © 2016 David de Tena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
