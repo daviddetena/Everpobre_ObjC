c91e72875914fd7751ee13262f1b41b4f9340d04
David de Tena <daviddetenacalvo@gmail.com>
2016-04-21 19:28:06 +0200

    Included KVO for class Notebook

--- Photo.h
+++ Photo.h
@@ -1,5 +1,14 @@
+@import UIKit;
 #import "_Photo.h"
 
 @interface Photo : _Photo
-// Custom logic goes here.
+
+#pragma mark - Properties
+// Transient property to deal with UIImage instead of NSData
+@property (nonatomic, strong) UIImage *image;
+
+
+#pragma mark - Class methods
++(instancetype) photoWithImage:(UIImage *) image context:(NSManagedObjectContext *) context;
+
 @end
--- Photo.m
+++ Photo.m
@@ -8,6 +8,29 @@
 
 @implementation Photo
 
-// Custom logic goes here.
+#pragma mark - Properties
+
+// Turn our transient property image into CoreData NSData
+- (void) setImage:(UIImage *)image{
+    self.imageData = UIImagePNGRepresentation(image);
+}
+
+// Custom getter with lazy loading
+- (UIImage *) image{
+    // Return a temp UIImage with its representation from CoreData (NSData)
+    return [UIImage imageWithData:self.imageData];
+}
+
+
+#pragma mark - Class Methods
++(instancetype) photoWithImage:(UIImage *) image
+                       context:(NSManagedObjectContext *) context{
+    
+    Photo *photo = [NSEntityDescription insertNewObjectForEntityForName:[Photo entityName]
+                                                 inManagedObjectContext: context];
+    
+    photo.imageData = UIImageJPEGRepresentation(image, 0.9);
+    return photo;
+}
 
 @end
