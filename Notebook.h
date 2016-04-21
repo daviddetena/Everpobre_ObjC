cee430dba805a889279aa28ca31d8e00cdc22353
David de Tena <daviddetenacalvo@gmail.com>
2016-04-21 09:38:25 +0200

    Added class methods for class Notebook

--- Notebook.h
+++ Notebook.h
@@ -2,4 +2,10 @@
 
 @interface Notebook : _Notebook
 // Custom logic goes here.
+
+#pragma mark - Class methods
+
++(instancetype) notebookWithName:(NSString *) name context:(NSManagedObjectContext *) context;
+
+
 @end
--- Notebook.m
+++ Notebook.m
@@ -8,6 +8,21 @@
 
 @implementation Notebook
 
-// Custom logic goes here.
+
+#pragma mark - Class methods
+
++(instancetype) notebookWithName:(NSString *) name
+                         context:(NSManagedObjectContext *) context{
+
+    // New notebook
+    Notebook *notebook = [NSEntityDescription insertNewObjectForEntityForName:[Notebook entityName]
+                                                       inManagedObjectContext:context];
+    
+    // Set dates
+    notebook.creationDate = [NSDate date];
+    notebook.modificationDate = [NSDate date];
+    
+    return notebook;
+}
 
 @end
