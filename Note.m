f37069e34b2c2fa5c1058c1c67bc499fa1132d79
David de Tena <daviddetenacalvo@gmail.com>
2016-04-21 09:46:08 +0200

    Added class method for Note

--- Note.m
+++ Note.m
@@ -8,6 +8,22 @@
 
 @implementation Note
 
-// Custom logic goes here.
+#pragma mark - Class methods
++(instancetype) noteWithName:(NSString *) name
+                    notebook:(Notebook *) notebook
+                     context:(NSManagedObjectContext *) context{
+
+
+    // New note
+    Note *note = [NSEntityDescription insertNewObjectForEntityForName:[Note entityName]
+                                                       inManagedObjectContext:context];
+    
+    // Set dates and notebook
+    note.creationDate = [NSDate date];
+    note.modificationDate = [NSDate date];
+    note.notebook = notebook;
+    
+    return note;
+}
 
 @end
